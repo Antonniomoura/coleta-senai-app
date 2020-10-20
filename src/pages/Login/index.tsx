3e3071757200c510edaf388ce71b9a95fa34b2e3
Antonniomoura <antonniogt@gmail.com>
2020-10-19 22:31:34 -0300

    fix: adjustment in develop

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
+
+const Home = () => {
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="Ecoleta" />
+        </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>
+            Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+          </p>
+
+          <div className="d-flex">
+            <Link to="/create-point">
+            <span>
+              <FiLogIn />
+            </span>
+              <strong>Cadastre um pronto de coleta</strong>
+            </Link>
+            <Link to="/points" className="ml-2">
+              <strong>Status de coletas</strong>
+            </Link>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
